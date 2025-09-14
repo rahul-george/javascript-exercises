569f3483a0d5aab9c27d34443a2f78a2f7622e88
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-09-14 20:14:14 +0000

    fix: fixed sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (typeof a !== "number" || typeof b != "number") return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  if (a - Math.floor(a) > 0 || b - Math.floor(b) > 0) return "ERROR";
 
+  let start = a > b ? b : a;
+  let end = a < b ? b : a;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
