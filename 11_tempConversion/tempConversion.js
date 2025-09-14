dd5371fc2e0ecda6ef6d942f0e9576dd06ea54aa
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-09-14 20:31:35 +0000

    fix: temp conversion algorithm fixed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  // (x-32)* 5/9
+  let temp = ((temperature - 32) * 5) / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let temp = (temperature * 9) / 5 + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
